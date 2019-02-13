2e9fb45eb28d9dbe2528169d54ee55bccd0a2941
Koneru <Rajesh.Koneru@McKesson.com>
2019-02-13 14:52:42 -0500

    my junk

--- main.py
+++ main.py
@@ -5,3 +5,22 @@
 # that new vector
 # find the norm of the final vector
 # also find the dot product between the 2nd and 3rd vector
+from vector import Vector
+a = []
+with open("vectors.txt") as f:
+    for line in f:
+        # lineitems = line.strip().split(',')
+        # #print(lineitems)
+        # lineitemsint = [int(i) for i in lineitems]
+        # #print(lineitemsint)
+        # v1= Vector(lineitemsint)
+        v = Vector(*tuple(map(int,line.strip().split(','))))
+        a.append(v)
+       
+for i in range(len(a)):
+    print(a[i])
+
+print(a[0].add(a[1]))
+print(a[0].scale(3))
+print(a[2].norm())
+print(a[0].product(a[2]))
--- vector.py
+++ vector.py
@@ -6,3 +6,32 @@
 # norm or len
 # dot product
 # override __str__ and __repr__
+from functools import reduce 
+class Vector:
+    def __init__(self, *args):
+        self.args = args
+        
+
+    def __str__(self):
+        return str(self.args)
+
+    def add(self,vect):
+        return Vector(*[x + y for x, y in zip(self.args, vect.args)])
+
+    def scale(self, dim1):
+        return Vector(*[x * dim1 for x in self.args])
+
+    def norm(self):
+        temp = reduce(lambda final, x: final + x**2,self.args)
+        return temp ** 0.5
+
+    def product(self,dim1):
+        return sum((x * y for x, y in zip(self.args, dim1.args)))
+
+
+
+
+    
+        
+
+        
